952e941f3f9bf3bf4327bae167a1925db7a925d2
Craig McClanahan <craigmcc@gmail.com>
2025-05-11 15:21:13 -0700

    Server actions for fetchAccessToken, fetchUser, and fetchUsers.

---
+++ apps/ramp-info/src/actions/UserActions.ts
@@ -0,0 +1,137 @@
+"use server";
+
+/**
+ * Server Actions for Users.
+ */
+
+// External Modules ----------------------------------------------------------
+
+// Internal Modules ----------------------------------------------------------
+
+import { RampResult, User, UsersResponse } from "@/types/Models";
+
+// Private Objects ------------------------------------------------------------
+
+const RAMP_PROD_API_BASE_URL = process.env.RAMP_PROD_API_BASE_URL;
+
+// Data Types ----------------------------------------------------------------
+
+/**
+ * Query parameters for fetchUsers().
+ */
+export type FetchUsersParams = {
+  // Filter by department id
+  department_id?: string;
+  // Filter by email
+  email?: string;
+  // Filter by employee_id
+  employee_id?: string;
+  // Filter by entity id
+  entity_id?: string;
+  // Filter by location id
+  location_id?: string;
+  // Number of results to return on each page (2-100). [20]
+  page_size?: number;
+  // Filter by role -- TODO - these do not match values in the role field
+  //role?: UserRole;
+  // The ID of the last entry on the previous page, for pagination
+  start?: string;
+}
+
+// Public Objects ------------------------------------------------------------
+
+/**
+ * Fetch a User by ID.
+ */
+export async function fetchUser(
+  // The access token to use for authentication
+  accessToken: string,
+  // The ID of the user to fetch
+  userId: string
+) {
+
+  if (!RAMP_PROD_API_BASE_URL) {
+    return {
+      error: {
+        error_code: "RAMP_PROD_API_BASE_URL_NOT_SET",
+        message: "RAMP_PROD_API_BASE_URL is not set",
+        status: 500
+      }
+    };
+  }
+
+  const url = new URL(`${RAMP_PROD_API_BASE_URL}/developer/v1/users/${userId}`);
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Authorization": `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    return {
+      error: { ...error, status: response.status },
+    };
+  }
+
+  const body = await response.json();
+  return {
+    headers: response.headers,
+    model: body as User,
+  }
+}
+
+/**
+ * Fetch all or selected users from the Ramp API.
+ */
+export async function fetchUsers(
+  // The access token to use for authentication
+  accessToken: string,
+  // Optional query parameters
+  params: FetchUsersParams = {}
+): Promise<RampResult<UsersResponse>> {
+
+  if (!RAMP_PROD_API_BASE_URL) {
+    return {
+      error: {
+        error_code: "RAMP_PROD_API_BASE_URL_NOT_SET",
+        message: "RAMP_PROD_API_BASE_URL is not set",
+        status: 500
+      }
+    };
+  }
+
+  const url = new URL(`${RAMP_PROD_API_BASE_URL}/developer/v1/users`);
+  Object.entries(params).forEach(([key, value]) => {
+    if (value) {
+      url.searchParams.append(key, value.toString());
+    }
+  });
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Authorization": `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    return {
+      error: { ...error, status: response.status },
+    };
+  }
+
+  const body = await response.json();
+  return {
+    headers: response.headers,
+    model: body as UsersResponse,
+  }
+
+}
---
+++ apps/ramp-info/src/types/Models.d.ts
@@ -0,0 +1,125 @@
+/**
+ * Types for Ramp API model objects.
+ */
+
+// Public Types --------------------------------------------------------------
+
+/**
+ * The "error_v2" portion of the Ramp response with an error, plus a spot for
+ * the HTTP status code.
+ */
+export type RampError = {
+  // Additional info about the error (if any)
+  additional_info?: {
+    [key: string]: string;
+  };
+  // The error code
+  error_code: string;
+  // The error message
+  message: string;
+  // Notes about the error
+  notes?: string;
+  // The HTTP status code
+  status: number;
+}
+
+/**
+ * Generic describing the result returned by a Ramp API call.
+ * Either an error or a model object will be included, but not both.
+ * The headers from the response are normally included.
+ *
+ * @param M                             The type of model object being returned
+ * @param E                             The type of error object being returned
+ *                                      (defaults to RampError)
+ */
+export type RampResult<M, E = RampError> = {
+  // The error object returned by the action (if any)
+  error?: E;
+  // The headers returned by the action (if any)
+  headers?: Headers;
+  // The model object returned by the action (if any)
+  model?: M;
+}
+
+/**
+ * The OAuth2 response for a successful token request.
+ */
+export type TokenResponse = {
+  // The access token
+  access_token: string;
+  // The type of token
+  token_type: string;
+  // The expiration time in seconds
+  expires_in: number;
+  // The scope of the token
+  scope: string;
+}
+
+/**
+ * A Ramp API User object.
+ */
+export type User = {
+  // Unique identifier of the company that the user belongs to
+  business_id: string | null;
+  // A list of custom fields for this user
+  custom_fields: {
+    [key: string]: string | number | boolean | null;
+  };
+  // Unique identifier of the user's department
+  department_id: string | null;
+  // The user's email address
+  email: string;
+  // Alternative identifier for an employee, which is from an external system,
+  // that can be used in place of an email
+  employee_id?: string;
+  // Unique identifier of the business entity that the user belongs to
+  entity_id: string | null;
+  // First name of the user
+  first_name?: string;
+  // Unique user identifier
+  id?: string;
+  // Is this user a manager?
+  is_manager: boolean;
+  // Last name of the user
+  last_name?: string;
+  // Unique identifier of the user's location
+  location_id: string | null;
+  // Unique identifier of the user's manager
+  manager_id: string | null;
+  // The user's phone number
+  phone?: string;
+  // The user's role
+  role?: UserRole;
+  // The user's status with respect to Ramp
+  status: UserStatus;
+}
+
+/**
+ * The Ramp API response for a fetch users request.
+ */
+export type UsersResponse = {
+  // The list of users
+  data: User[];
+  // Optional forward link for pagination
+  page?: {
+    next?: string;
+  }
+}
+
+/**
+ * Role of a User
+ */
+export type UserRole =
+  "ADVISOR_CONSOLE_ADMIN" | "ADVISOR_CONSOLE_USER" | "AUDITOR" |
+  "BUSINESS_ADMIN" | "BUSINESS_BOOKKEEPER" | "BUSINESS_OWNER" |
+  "BUSINESS_USER" | "DEVELOPER_ADMIN" | "GUEST_USER" | "IT_ADMIN" |
+  "PRESALES_DEMO_USER" | "UNBUNDLED_ADMIN" | "UNBUNDLED_BOOKKEEPER" |
+  "UNBUNDLED_OWNER" | "UNBUNDLED_USER" | "VENDOR_NETWORK_ADMIN";
+
+/**
+ * Status of a User.
+ */
+export type UserStatus =
+  "INVITE_EXPIRED" | "INVITE_PENDING" | "USER_ACTIVE" |
+  "USER_INACTIVE" | "USER_ONBOARDING" | "USER_SUSPENDED";
+
