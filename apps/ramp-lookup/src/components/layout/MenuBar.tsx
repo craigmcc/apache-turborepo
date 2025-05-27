df49511f21a678b9261964129073639a8259221b
Craig McClanahan <craigmcc@gmail.com>
2025-05-26 23:54:47 -0400

    Beginnings of the actual menu bar. DONE: Replace unknown card_ids on transactions with a known dummy card_id, so that at least we capture the data plus the violation. TODO: Need to do the same for bad user ids; TODO: Populate subtables for transactions as needed.

--- apps/ramp-lookup/src/components/layout/MenuBar.tsx
+++ apps/ramp-lookup/src/components/layout/MenuBar.tsx
@@ -7,31 +7,59 @@
 // External Imports ----------------------------------------------------------
 
 import { Images } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 // Internal Imports ----------------------------------------------------------
 
 // Public Objects ------------------------------------------------------------
 
 export function MenuBar() {
+
+  const [key, setKey] = useState<string | null>(null);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          <Images className="pe-2" size={38} />
-          RAMP Lookup
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      </div>
-    </nav>
+    <Container className="bg-light" fluid>
+      <Row className="my-2">
+        <Col className="mt-1">
+          <Link href="/">
+            <Images className="pe-2" size={38}/>
+            RAMP Lookup
+          </Link>
+        </Col>
+        <Col className="w-100">
+          <Tabs
+            activeKey={key ? key : undefined}
+            fill
+            id="ramp-lookup-tabs"
+            onSelect={(k) => setKey(k)}
+          >
+            <Tab
+              eventKey="Departments"
+              title="Departments"
+            >
+              TODO - redirect to departments page
+            </Tab>
+            <Tab
+              eventKey="Users"
+              title="Users"
+            >
+              TODO - redirect to users page
+            </Tab>
+            <Tab
+              eventKey="Cards"
+              title="Cards"
+            >
+              TODO - redirect to cards page
+            </Tab>
+            </Tabs>
+        </Col>
+      </Row>
+    </Container>
   );
 }
