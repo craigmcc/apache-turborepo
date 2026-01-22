a415718ce780c66304ae8f179d3db4049b812aaf
Craig McClanahan <craigmcc@gmail.com>
2026-01-22 11:57:36 -0800

    Completed Node app to retrieve a JournalReport.
    Lots of TODOs.
    
    TODO: Review "department head" choices.

--- apps/qbo-report-journal/src/index.ts
+++ apps/qbo-report-journal/src/index.ts
@@ -59,25 +59,52 @@ async function main() {
     throw new Error("endDate must be on or after startDate");
   }
   log({
-    context: "qbo-report-journal.started",
+    context: "qbo-report-journal.parameters",
     startDate: args.values.startDate,
     endDate: args.values.endDate,
+    to: args.values.to,
   });
 
-  // Execute the report request
+  // Set up the report request
   const apiInfo = await fetchApiInfo(API_TIMEOUT);
-  // ...
-  const result = {
-    report: "journal",
-    startDate: args.values.startDate,
-    endDate: args.values.endDate,
-  }
+  const url = new URL(`${apiInfo.baseUrl}/v3/company/${apiInfo.realmId}/reports/JournalReport`);
+  // Accept the default columns for now
+  url.searchParams.set("end_date", args.values.endDate);
+  url.searchParams.set("sort_by", "acct_num_with_extn,tx_date");
+  url.searchParams.set("sort_order", "ASCENDING");
+  url.searchParams.set("start_date", args.values.startDate);
 
-  // Send the result to the specified destination
+  // Perform the report request
+  log({
+    context: "qbo-report-journal.requesting",
+    url: url.toString(),
+  });
+  const response = await fetch(url, {
+    headers: {
+      "Accept": "application/json",
+      "Authorization": `Bearer ${apiInfo.accessToken}`,
+    }
+  });
+  if (!response.ok) {
+    log({
+      context: "qbo-report-journal.error",
+      body: response.body,
+      status: response.status,
+    });
+    const text = JSON.stringify(response.body, null, 2);
+    throw new Error(`Error fetching Journal report: ${text}`);
+  }
+  const json = await response.json();
+  log({
+    context: "qbo-report-journal.received",
+    status: response.status,
+  });
+
+  // Forward the response data to the output
   if (args.values.to) {
-    fs.writeFileSync(args.values.to, JSON.stringify(result, null, 2) + "\n");
+    fs.writeFileSync(args.values.to, JSON.stringify(json, null, 2) + "\n");
   } else {
-    process.stdout.write(JSON.stringify(result, null, 2) + "\n");
+    process.stdout.write(JSON.stringify(json, null, 2) + "\n");
   }
 
 }
