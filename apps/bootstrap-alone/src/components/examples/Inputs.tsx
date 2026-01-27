dfb9fe3f2a36968f6ae23c6cda31e8bb9ec120e0
Craig McClanahan <craigmcc@gmail.com>
2026-01-26 19:55:21 -0800

    Refine the layout of the Inputs component to adjust widths but keep responsiveness.

--- apps/bootstrap-alone/src/components/examples/Inputs.tsx
+++ apps/bootstrap-alone/src/components/examples/Inputs.tsx
@@ -12,6 +12,9 @@ import {
   CardBody,
   CardText,
   CardTitle,
+  Col,
+  Container,
+  Row,
   Table,
 } from "react-bootstrap";
 import { FieldCheckbox } from "@repo/bootstrap-tanstack-form/FieldCheckbox";
@@ -37,144 +40,149 @@ export function Inputs() {
   ];
 
   return (
-    <div className="d-flex justify-content-evenly gap-4">
+    <Container fluid>
+      <Row className="g-4">
+        <Col xs={12} lg={7}>
+          <Card className="border bg-info-subtle h-100">
+            <CardBody>
+              <CardTitle className="text-center">Horizontal Inputs</CardTitle>
+              <CardText as="div">
+                <Table border={2} bordered>
+                  <thead>
+                  <tr>
+                    <th>Input Type</th>
+                    <th className="text-center">Example</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td className="font-weight-medium">Checkbox</td>
+                    <td className="text-center">
+                      <FieldCheckbox
+                        handleChange={(newValue) => setExampleCheckbox(newValue)}
+                        label="Example Checkbox"
+                        name="exampleCheckbox"
+                        value={exampleCheckbox}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="font-weight-medium">Password</td>
+                    <td className="form-control">
+                      <FieldInput
+                        handleChange={(newValue) => setExamplePassword(newValue)}
+                        horizontal={4}
+                        label="Example Password:"
+                        name="examplePassword"
+                        placeholder="Enter password"
+                        type="password"
+                        value={examplePassword}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="font-weight-medium">Select</td>
+                    <td className="form-control">
+                      <FieldSelect
+                        handleChange={(newValue) => setExampleSelect(newValue)}
+                        horizontal={4}
+                        label="Example Select:"
+                        name="exampleSelect"
+                        options={selectOptions}
+                        value={exampleSelect}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="font-weight-medium">Text</td>
+                    <td className="form-control">
+                      <FieldInput
+                        handleChange={(newValue) => setExampleInput(newValue)}
+                        horizontal={4}
+                        label="Example Text:"
+                        name="exampleInput"
+                        placeholder="Enter text"
+                        value={exampleInput}
+                      />
+                    </td>
+                  </tr>
+                  </tbody>
+                </Table>
+              </CardText>
+            </CardBody>
+          </Card>
+        </Col>
 
-      <Card className="border bg-info-subtle">
-        <CardBody>
-          <CardTitle className="text-center">Horizontal Inputs</CardTitle>
-          <CardText as="div">
-            <Table border={2} bordered>
-              <thead>
-              <tr>
-                <th>Input Type</th>
-                <th className="text-center">Example</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td className="font-weight-medium">Checkbox</td>
-                <td className="text-center">
-                  <FieldCheckbox
-                    handleChange={(newValue) => setExampleCheckbox(newValue)}
-                    label="Example Checkbox"
-                    name="exampleCheckbox"
-                    value={exampleCheckbox}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="font-weight-medium">Password</td>
-                <td className="form-control">
-                  <FieldInput
-                    handleChange={(newValue) => setExamplePassword(newValue)}
-                    horizontal={4}
-                    label="Password:"
-                    name="examplePassword"
-                    placeholder="Enter password"
-                    type="password"
-                    value={examplePassword}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="font-weight-medium">Select</td>
-                <td className="form-control">
-                  <FieldSelect
-                    handleChange={(newValue) => setExampleSelect(newValue)}
-                    horizontal={4}
-                    label="Select:"
-                    name="exampleSelect"
-                    options={selectOptions}
-                    value={exampleSelect}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="font-weight-medium">Text</td>
-                <td className="form-control">
-                  <FieldInput
-                    handleChange={(newValue) => setExampleInput(newValue)}
-                    horizontal={4}
-                    label="Text:"
-                    name="exampleInput"
-                    placeholder="Enter text"
-                    value={exampleInput}
-                  />
-                </td>
-              </tr>
-              </tbody>
-            </Table>
-          </CardText>
-        </CardBody>
-      </Card>
+        <Col xs={12} lg={5}>
+          <Card className="border bg-info-subtle h-100">
+            <CardBody>
+              <CardTitle className="text-center">Vertical Inputs</CardTitle>
+              <CardText as="div">
+                <Table border={2} bordered>
+                  <thead>
+                  <tr>
+                    <th>Input Type</th>
+                    <th className="text-center">Example</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td className="font-weight-medium">Checkbox</td>
+                    <td className="text-center">
+                      <FieldCheckbox
+                        handleChange={(newValue) => setExampleCheckbox(newValue)}
+                        label="Example Checkbox"
+                        name="exampleCheckbox"
+                        value={exampleCheckbox}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="font-weight-medium">Password</td>
+                    <td className="form-control">
+                      <FieldInput
+                        handleChange={(newValue) => setExamplePassword(newValue)}
+                        label="Example Password:"
+                        name="examplePassword"
+                        placeholder="Enter password"
+                        type="password"
+                        value={examplePassword}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="font-weight-medium">Select</td>
+                    <td className="form-control">
+                      <FieldSelect
+                        handleChange={(newValue) => setExampleSelect(newValue)}
+                        label="Example Select:"
+                        name="exampleSelect"
+                        options={selectOptions}
+                        value={exampleSelect}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="font-weight-medium">Text</td>
+                    <td className="form-control">
+                      <FieldInput
+                        handleChange={(newValue) => setExampleInput(newValue)}
+                        label="Example Text:"
+                        name="exampleInput"
+                        placeholder="Enter text"
+                        value={exampleInput}
+                      />
+                    </td>
+                  </tr>
+                  </tbody>
+                </Table>
+              </CardText>
+            </CardBody>
+          </Card>
 
-      <Card className="border bg-info-subtle">
-        <CardBody>
-          <CardTitle className="text-center">Vertical Inputs</CardTitle>
-          <CardText as="div">
-            <Table border={2} bordered>
-              <thead>
-              <tr>
-                <th>Input Type</th>
-                <th className="text-center">Example</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td className="font-weight-medium">Checkbox</td>
-                <td className="text-center">
-                  <FieldCheckbox
-                    handleChange={(newValue) => setExampleCheckbox(newValue)}
-                    label="Example Checkbox"
-                    name="exampleCheckbox"
-                    value={exampleCheckbox}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="font-weight-medium">Password</td>
-                <td className="form-control">
-                  <FieldInput
-                    handleChange={(newValue) => setExamplePassword(newValue)}
-                    label="Password:"
-                    name="examplePassword"
-                    placeholder="Enter password"
-                    type="password"
-                    value={examplePassword}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="font-weight-medium">Select</td>
-                <td className="form-control">
-                  <FieldSelect
-                    handleChange={(newValue) => setExampleSelect(newValue)}
-                    label="Select:"
-                    name="exampleSelect"
-                    options={selectOptions}
-                    value={exampleSelect}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="font-weight-medium">Text</td>
-                <td className="form-control">
-                  <FieldInput
-                    handleChange={(newValue) => setExampleInput(newValue)}
-                    label="Text:"
-                    name="exampleInput"
-                    placeholder="Enter text"
-                    value={exampleInput}
-                  />
-                </td>
-              </tr>
-              </tbody>
-            </Table>
-          </CardText>
-        </CardBody>
-      </Card>
-
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 
 }
